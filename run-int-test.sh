626f703366180a3cc992cd9882b3c3262e4cfab5
Miran Kurukulasuriya <miran@wso2.com>
2026-02-25 07:27:38 +0530

    Update user store configuration in integration test script
    Modify integration test script to update user store configuration for version 5.11.0.

--- run-int-test.sh
+++ run-int-test.sh
@@ -186,6 +186,9 @@ if [[ "$PRODUCT_VERSION" != *"SNAPSHOT"* ]]; then
         log_info "Add WSO2 repository to pom.xml"
         
         bash $TESTGRID_DIR/$PRODUCT_REPOSITORY_NAME/add-patch-repository.sh
+        if [[ "$PRODUCT_VERSION" == "5.11.0" ]]
+            cd $TESTGRID_DIR/$PRODUCT_REPOSITORY_NAME
+            find . -name "*.toml" -type f -exec sed -i '/^\[user_store\]/,/^base_dn =/c\[user_store]\ntype = "database_unique_id"\n#connection_url = "ldap://localhost:${Ports.EmbeddedLDAP.LDAPServerPort}"\n#connection_name = "uid=admin,ou=system"\n#connection_password = "admin"\n#base_dn = "dc=wso2,dc=org"' {} +
     fi
     log_info "Running Maven clean install"
     #For Tag-based execution we initially build the product pack and then run the integration tests
