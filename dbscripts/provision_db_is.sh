836f1619aa489018c6f4e91898d20b7da19a3435
Miran Kurukulasuriya <miran@wso2.com>
2026-03-16 10:11:18 +0530

    Rename database schemas

--- dbscripts/provision_db_is.sh
+++ dbscripts/provision_db_is.sh
@@ -93,17 +93,17 @@ if [[ $DB_ENGINE = "postgres" ]]; then
     psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d postgres -f /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_postgres_db_create.sql
     
     # Step 2: Populate each database with its schema
-    echo "Populating WSO2IS_SHARED_DB schema..."
-    psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d WSO2IS_SHARED_DB -f /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_postgres_shared.sql
+    echo "Populating WSO2SHARED_DB schema..."
+    psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d WSO2SHARED_DB -f /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_postgres_shared.sql
     
-    echo "Populating WSO2IS_IDENTITY_DB schema..."
-    psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d WSO2IS_IDENTITY_DB -f /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_postgres_identity.sql
+    echo "Populating WSO2IDENTITY_DB schema..."
+    psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d WSO2IDENTITY_DB -f /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_postgres_identity.sql
     
-    echo "Populating WSO2IS_CONSENT_DB schema..."
-    psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d WSO2IS_CONSENT_DB -f /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_postgres_consent.sql
+    echo "Populating WSO2CONSENT_DB schema..."
+    psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d WSO2CONSENT_DB -f /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_postgres_consent.sql
     
-    echo "Populating WSO2IS_AGENTIDENTITY_DB schema..."
-    psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d WSO2IS_AGENTIDENTITY_DB -f /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_postgres_agent_identity.sql
+    echo "Populating WSO2AGENTIDENTITY_DB schema..."
+    psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d WSO2AGENTIDENTITY_DB -f /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_postgres_agent_identity.sql
 elif [[ $DB_ENGINE = "mysql" ]]; then
     # DB Engine : MySQL
     echo "MySQL DB Engine Selected! Running WSO2-IS $WSO2_PRODUCT_VERSION DB Scripts for MySQL..."
@@ -113,17 +113,17 @@ elif [[ $DB_ENGINE = "mysql" ]]; then
     mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mysql_db_create.sql
     
     # Step 2: Populate each database with its schema
-    echo "Populating WSO2IS_SHARED_DB schema..."
-    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2IS_SHARED_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mysql_shared.sql
+    echo "Populating WSO2SHARED_DB schema..."
+    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2SHARED_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mysql_shared.sql
     
-    echo "Populating WSO2IS_IDENTITY_DB schema..."
-    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2IS_IDENTITY_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mysql_identity.sql
+    echo "Populating WSO2IDENTITY_DB schema..."
+    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2IDENTITY_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mysql_identity.sql
     
-    echo "Populating WSO2IS_CONSENT_DB schema..."
-    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2IS_CONSENT_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mysql_consent.sql
+    echo "Populating WSO2CONSENT_DB schema..."
+    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2CONSENT_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mysql_consent.sql
     
-    echo "Populating WSO2IS_AGENTIDENTITY_DB schema..."
-    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2IS_AGENTIDENTITY_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mysql_agent_identity.sql
+    echo "Populating WSO2AGENTIDENTITY_DB schema..."
+    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2AGENTIDENTITY_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mysql_agent_identity.sql
 elif [[ $DB_ENGINE = "mariadb" ]]; then
     # DB Engine : mariadb
     echo "Maria DB Engine Selected! Running WSO2-IS $WSO2_PRODUCT_VERSION DB Scripts for MariaDB..."
@@ -133,30 +133,30 @@ elif [[ $DB_ENGINE = "mariadb" ]]; then
     mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mariadb_db_create.sql
     
     # Step 2: Populate each database with its schema
-    echo "Populating WSO2IS_SHARED_DB schema..."
-    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2IS_SHARED_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mariadb_shared.sql
+    echo "Populating WSO2SHARED_DB schema..."
+    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2SHARED_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mariadb_shared.sql
     
-    echo "Populating WSO2IS_IDENTITY_DB schema..."
-    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2IS_IDENTITY_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mariadb_identity.sql
+    echo "Populating WSO2IDENTITY_DB schema..."
+    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2IDENTITY_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mariadb_identity.sql
     
-    echo "Populating WSO2IS_CONSENT_DB schema..."
-    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2IS_CONSENT_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mariadb_consent.sql
+    echo "Populating WSO2CONSENT_DB schema..."
+    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2CONSENT_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mariadb_consent.sql
     
-    echo "Populating WSO2IS_AGENTIDENTITY_DB schema..."
-    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2IS_AGENTIDENTITY_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mariadb_agent_identity.sql
+    echo "Populating WSO2AGENTIDENTITY_DB schema..."
+    mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT WSO2AGENTIDENTITY_DB < /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mariadb_agent_identity.sql
 elif [[ $DB_ENGINE =~ 'oracle-se' ]]; then
     # DB Engine : Oracle
     echo "Oracle DB Engine Selected! Running WSO2-IS $WSO2_PRODUCT_VERSION DB Scripts for Oracle..."
     # Create users to the required DB
     echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2IS_BPS_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2IS_BPS_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
-    echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2IS_IDENTITY_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2IS_IDENTITY_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
-    echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2IS_CONSENT_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2IS_CONSENT_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
+    echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2IDENTITY_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2IDENTITY_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
+    echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2CONSENT_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2CONSENT_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
     echo "CREATE USER WSO2IS_BPS_DB IDENTIFIED BY CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2IS_BPS_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2IS_BPS_DB;" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
-    echo "CREATE USER WSO2IS_IDENTITY_DB IDENTIFIED BY CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2IS_IDENTITY_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2IS_IDENTITY_DB;" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
-    echo "CREATE USER WSO2IS_CONSENT_DB IDENTIFIED BY CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2IS_CONSENT_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2IS_CONSENT_DB;" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
+    echo "CREATE USER WSO2IDENTITY_DB IDENTIFIED BY CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2IDENTITY_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2IDENTITY_DB;" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
+    echo "CREATE USER WSO2CONSENT_DB IDENTIFIED BY CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2CONSENT_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2CONSENT_DB;" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
     if [[ $WSO2_PRODUCT_VERSION = "5.9.0" || $WSO2_PRODUCT_VERSION = "5.10.0" || $WSO2_PRODUCT_VERSION = "5.11.0" || $WSO2_PRODUCT_VERSION = "7.0.0" ]]; then
-        echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2IS_SHARED_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2IS_REG_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
-        echo "CREATE USER WSO2IS_SHARED_DB IDENTIFIED BY CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2IS_SHARED_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2IS_SHARED_DB;" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
+        echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2SHARED_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2IS_REG_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
+        echo "CREATE USER WSO2SHARED_DB IDENTIFIED BY CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2SHARED_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2SHARED_DB;" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
     else
         echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2IS_REG_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2IS_REG_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
         echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2IS_USER_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2IS_USER_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle.sql
@@ -172,21 +172,21 @@ elif [[ $DB_ENGINE =~ 'oracle-se' ]]; then
     echo exit | sqlplus64 WSO2IS_BPS_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_bps.sql
     fi
     echo "--------------------IDENTITY---------------------"
-    echo exit | sqlplus64 WSO2IS_IDENTITY_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_identity.sql
+    echo exit | sqlplus64 WSO2IDENTITY_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_identity.sql
     if [[ $WSO2_PRODUCT_VERSION = "5.10.0" || $WSO2_PRODUCT_VERSION = "5.11.0" || $WSO2_PRODUCT_VERSION = "7.0.0" || $WSO2_PRODUCT_VERSION != "7.1.0-SNAPSHOT" || $WSO2_PRODUCT_VERSION != "7.1.0" || $WSO2_PRODUCT_VERSION != "7.2.0-SNAPSHOT" || $WSO2_PRODUCT_VERSION != "7.2.0" || $WSO2_PRODUCT_VERSION != *"7.2.1"* || $WSO2_PRODUCT_VERSION != *"7.3.0"* ]]; then
     echo "--------------------COMMON---------------------"
-        echo exit | sqlplus64 WSO2IS_SHARED_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_common.sql
+        echo exit | sqlplus64 WSO2SHARED_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_common.sql
     elif [[ $WSO2_PRODUCT_VERSION = "5.9.0" ]]; then
     echo "--------------------COMMON---------------------"
-        echo exit | sqlplus64 WSO2IS_SHARED_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_common.sql
+        echo exit | sqlplus64 WSO2SHARED_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_common.sql
     else
         echo exit | sqlplus64 WSO2IS_REG_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_common.sql
         echo exit | sqlplus64 WSO2IS_USER_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_common.sql
     fi
     if $USE_CONSENT_DB; then
-        echo exit | sqlplus64 WSO2IS_CONSENT_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_consent.sql
+        echo exit | sqlplus64 WSO2CONSENT_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_consent.sql
     else
-        echo exit | sqlplus64 WSO2IS_CONSENT_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_common.sql
+        echo exit | sqlplus64 WSO2CONSENT_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2ISDB @/opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_oracle_common.sql
     fi
 elif [[ $DB_ENGINE =~ 'sqlserver-se' ]]; then
     # DB Engine : SQLServer
@@ -197,16 +197,16 @@ elif [[ $DB_ENGINE =~ 'sqlserver-se' ]]; then
     sqlcmd -S CF_DB_HOST -U CF_DB_USERNAME -P CF_DB_PASSWORD -i /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mssql_db_create.sql
     
     # Step 2: Populate each database with its schema (USE DATABASE statement included in schema files)
-    echo "Populating WSO2IS_SHARED_DB schema..."
+    echo "Populating WSO2SHARED_DB schema..."
     sqlcmd -S CF_DB_HOST -U CF_DB_USERNAME -P CF_DB_PASSWORD -i /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mssql_shared.sql
     
-    echo "Populating WSO2IS_IDENTITY_DB schema..."
+    echo "Populating WSO2IDENTITY_DB schema..."
     sqlcmd -S CF_DB_HOST -U CF_DB_USERNAME -P CF_DB_PASSWORD -i /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mssql_identity.sql
     
-    echo "Populating WSO2IS_CONSENT_DB schema..."
+    echo "Populating WSO2CONSENT_DB schema..."
     sqlcmd -S CF_DB_HOST -U CF_DB_USERNAME -P CF_DB_PASSWORD -i /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mssql_consent.sql
     
-    echo "Populating WSO2IS_AGENTIDENTITY_DB schema..."
+    echo "Populating WSO2AGENTIDENTITY_DB schema..."
     sqlcmd -S CF_DB_HOST -U CF_DB_USERNAME -P CF_DB_PASSWORD -i /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_mssql_agent_identity.sql
 elif [[ $DB_ENGINE = "db2-se" ]]; then
     # DB Engine : DB2
@@ -217,23 +217,23 @@ elif [[ $DB_ENGINE = "db2-se" ]]; then
     db2cli execsql -execute -dsn CF_DB_NAME -u CF_DB_USERNAME -p CF_DB_PASSWORD -h CF_DB_HOST -p CF_DB_PORT -inputsql /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_db2_db_create.sql -outfile /tmp/db2_create_output.log -statementdelimiter ";" -commentstart "--"
     
     # Step 2: Populate each database with its schema
-    echo "Populating WSO2IS_SHARED_DB schema..."
-    db2 connect to WSO2IS_SHARED_DB user CF_DB_USERNAME using CF_DB_PASSWORD
+    echo "Populating WSO2SHARED_DB schema..."
+    db2 connect to WSO2SHARED_DB user CF_DB_USERNAME using CF_DB_PASSWORD
     db2 -tvf /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_db2_shared.sql
     db2 connect reset
     
-    echo "Populating WSO2IS_IDENTITY_DB schema..."
-    db2 connect to WSO2IS_IDENTITY_DB user CF_DB_USERNAME using CF_DB_PASSWORD
+    echo "Populating WSO2IDENTITY_DB schema..."
+    db2 connect to WSO2IDENTITY_DB user CF_DB_USERNAME using CF_DB_PASSWORD
     db2 -tvf /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_db2_identity.sql
     db2 connect reset
     
-    echo "Populating WSO2IS_CONSENT_DB schema..."
-    db2 connect to WSO2IS_CONSENT_DB user CF_DB_USERNAME using CF_DB_PASSWORD
+    echo "Populating WSO2CONSENT_DB schema..."
+    db2 connect to WSO2CONSENT_DB user CF_DB_USERNAME using CF_DB_PASSWORD
     db2 -tvf /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_db2_consent.sql
     db2 connect reset
     
-    echo "Populating WSO2IS_AGENTIDENTITY_DB schema..."
-    db2 connect to WSO2IS_AGENTIDENTITY_DB user CF_DB_USERNAME using CF_DB_PASSWORD
+    echo "Populating WSO2AGENTIDENTITY_DB schema..."
+    db2 connect to WSO2AGENTIDENTITY_DB user CF_DB_USERNAME using CF_DB_PASSWORD
     db2 -tvf /opt/testgrid/workspace/$WSO2_PRODUCT_VERSION_SHORT/is_db2_agent_identity.sql
     db2 connect reset
 fi
