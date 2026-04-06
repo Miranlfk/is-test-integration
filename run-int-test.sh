33769751af8874b69b295b480e46cfa709f76516
Miran Kurukulasuriya <miran@wso2.com>
2026-04-06 10:30:14 +0530

    Fix PRODUCT_VERSION variable in run-int-test.sh
    Updated PRODUCT_VERSION variable for correct file naming.

--- run-int-test.sh
+++ run-int-test.sh
@@ -216,8 +216,8 @@ if [[ "$PRODUCT_VERSION" != *"SNAPSHOT"* ]]; then
         find $PRODUCT_REPOSITORY_PACK_DIR -name "*SNAPSHOT*" -type f -delete
         log_info "SNAPSHOT packs removed from $PRODUCT_REPOSITORY_PACK_DIR"
         echo "Copying pack to target"
-        PRODUCT_VERSION="7.3.0-rc1-SNAPSHOT"
-        mv $TESTGRID_DIR/$PRODUCT_NAME-$PRODUCT_VERSION.zip $PRODUCT_REPOSITORY_PACK_DIR/$PRODUCT_NAME-$PRODUCT_VERSION.zip
+        PRODUCT_VERSION1="7.3.0-rc1-SNAPSHOT"
+        mv $TESTGRID_DIR/$PRODUCT_NAME-$PRODUCT_VERSION.zip $PRODUCT_REPOSITORY_PACK_DIR/$PRODUCT_NAME-$PRODUCT_VERSION1.zip
     else
         log_info "No SNAPSHOT packs found in $PRODUCT_REPOSITORY_PACK_DIR, skipping removal"
         echo "Copying pack to target"
